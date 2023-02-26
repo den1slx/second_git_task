2a79c05bf669199b15d8dea6b5088e4b8066610b
den1slx <121083421+den1slx@users.noreply.github.com>
2023-02-26 22:37:00 +1000

    Publish photo to Telegram channel

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import os
 import telegram
 
 
-token = os.environ['TG_TOKEN']
-bot = telegram.Bot(token=token)
-chat_id = '@channerfortest'
+def main():
+    token = os.environ['TG_TOKEN']
+    bot = telegram.Bot(token=token)
+    chat_id = '@channerfortest'
+    path = r'C:\Users\Денис\Projects\devman tasks\second_git_task\new_folder\apod_images'
+    image = '2016-12-11T173502JunoPerijove3Peach1024.jpg'
+    with open(f'{path}\\{image}', 'rb') as foto_1:
+        bot.send_photo(chat_id=chat_id, photo=foto_1)
 
-bot.send_message(chat_id=chat_id, text='Hello word')
+
+if __name__ == '__main__':
+    main()
