e9851be672e4b3260104071f8d3fa02a36f9c3e9
den1slx <121083421+den1slx@users.noreply.github.com>
2023-02-26 13:20:32 +1000

    Create fetch_spacex_images.py

---
+++ fetch_spacex_images.py
@@ -0,0 +1,47 @@
+import requests
+import argparse
+from downloader import downloader, get_file_extend
+
+
+def fetch_spacex_last_launch(
+    path,
+    launch_id,
+    name='SpaceX_',
+):
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        links = response.json()['links']['flickr']['original']
+        for index, link in enumerate(links):
+            extend = get_file_extend(link)
+            downloader(path, link, name=f'{name}{index}', extend=extend)
+    except requests.exceptions.HTTPError:
+        pass
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'path',
+        help='your path where to save',
+    )
+    parser.add_argument(
+        '--id',
+        help='launch id',
+        default='latest'
+    )
+    return parser
+
+
+def main():
+    parser = create_parser()
+    spacename = parser.parse_args()
+    fetch_spacex_last_launch(
+        spacename.path,
+        spacename.id,
+    )
+
+
+if __name__ == '__main__':
+    main()
