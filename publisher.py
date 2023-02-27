c2ea6fec8f5231c0b5e7a48597a1b74cc5f4065e
den1slx <121083421+den1slx@users.noreply.github.com>
2023-02-27 11:12:51 +1000

    Update publisher: added arg 'period'

--- publisher.py
+++ publisher.py
@@ -6,17 +6,30 @@ import argparse
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('name', help='file name')
+    parser.add_argument(
+        'name',
+        help='file name',
+    )
+    parser.add_argument(
+        '-p',
+        '--period',
+        help='If not indicated: ignore files without period in name, where name is file_name+file_extend.',
+        action='store_true',
+    )
     return parser
 
 
-def publisher(token, chat_id, path, image):
+def publisher(token, chat_id, path, image, period=True):
     bot = telegram.Bot(token=token)
-    if '.' in image:
+    if period is True:
+        if '.' in image:
+            with open(f'{path}\\{image}', 'rb') as foto:
+                bot.send_photo(chat_id=chat_id, photo=foto)
+        else:
+            pass
+    else:
         with open(f'{path}\\{image}', 'rb') as foto:
             bot.send_photo(chat_id=chat_id, photo=foto)
-    else:
-        pass
 
 
 def main():
@@ -26,7 +39,7 @@ def main():
     token = os.environ['TG_TOKEN']
     chat_id = os.environ['CHAT_ID']
     path = os.environ['PATH_TO_FILES']
-    publisher(token, chat_id, path, namespace.name)
+    publisher(token, chat_id, path, namespace.name, namespace.period)
 
 
 if __name__ == '__main__':
