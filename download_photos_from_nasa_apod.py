7764855974015a66761aef66c266cc81014ff0a3
den1slx <121083421+den1slx@users.noreply.github.com>
2023-03-02 23:35:50 +1000

    Change funcs , parser and args, add get_headers

--- download_photos_from_nasa_apod.py
+++ download_photos_from_nasa_apod.py
@@ -14,13 +14,13 @@ def get_current_date():
     return today
 
 
-def get_images_at_quantity_from_apod(path, token, count, hd=False):
-    url = 'https://api.nasa.gov/planetary/apod'
-    headers = {
-        'api_key': token,
-        'count': count,
-    }
-    mod = get_hd_mod(hd)
+def get_headers(**kwargs):
+    headers = kwargs
+    return headers
+
+
+def get_images_from_apod(path, headers, url, boolean_hd=False):
+    mod = get_hd_mod(boolean_hd)
     path = PurePath(path).joinpath('apod_images').joinpath(mod)
     response = requests.get(url, headers)
     response.raise_for_status()
@@ -36,13 +36,8 @@ def get_images_at_quantity_from_apod(path, token, count, hd=False):
         )
 
 
-def get_image_from_apod_by_date(path, token, date, hd=False):
-    url = 'https://api.nasa.gov/planetary/apod'
-    headers = {
-        'api_key': token,
-        'date': date,
-    }
-    mod = get_hd_mod(hd)
+def get_image_at_date_from_apod(path, headers, url, boolean_hd=False):
+    mod = get_hd_mod(boolean_hd)
     path = PurePath(path).joinpath('apod_images').joinpath(mod)
     response = requests.get(url, headers)
     response.raise_for_status()
@@ -56,31 +51,9 @@ def get_image_from_apod_by_date(path, token, date, hd=False):
     )
 
 
-def get_images_from_apod_from_date_to_date(path, token, start_date=None, end_date=get_current_date(), hd=False):
-    url = 'https://api.nasa.gov/planetary/apod'
-    headers = {
-        'api_key': token,
-        'start_date': start_date,
-        'end_date': end_date,
-    }
-    mod = get_hd_mod(hd)
-    path = PurePath(path).joinpath('apod_images').joinpath(mod)
-    response = requests.get(url, headers)
-    response.raise_for_status()
-    photos = response.json()
-    for photo in photos:
-        name, extension = get_name_and_extension_file(photo[f'{mod}url'])
-        fetch_file(
-            path,
-            photo[f'{mod}url'],
-            name=f'{mod}{name}',
-            extension=extension,
-        )
-
-
-def get_hd_mod(hd):
+def get_hd_mod(boolean):
     mod = ''
-    if hd:
+    if boolean:
         mod = 'hd'
     return mod
 
@@ -111,8 +84,8 @@ def create_parser():
     parser.add_argument(
         '-ed',
         '--end_date',
-        help='end_date used with start_date. default end_date=today',
-        default=get_current_date(),
+        help='end_date used with start_date. default today',
+        default=None,
     )
     parser.add_argument(
         '--hd',
@@ -126,17 +99,25 @@ def main():
     load_dotenv()
     token = os.environ['NASA_TOKEN']
     path = os.environ['PATH_TO_FILES']
+    url = 'https://api.nasa.gov/planetary/apod'
     parser = create_parser()
     namespace = parser.parse_args()
     count, hd = namespace.count, namespace.hd
     date, start_date, end_date = namespace.date, namespace.start_date, namespace.end_date
+    if start_date:
+        end_date = get_current_date()
+    headers = get_headers(
+        api_key=token,
+        count=count,
+        date=date,
+        start_date=start_date,
+        end_date=end_date,
+    )
     with suppress(requests.exceptions.HTTPError):
-        if count:
-            get_images_at_quantity_from_apod(path, token, count, hd=hd)
-        elif date:
-            get_image_from_apod_by_date(path, token, date, hd=hd)
-        elif start_date:
-            get_images_from_apod_from_date_to_date(path, token, start_date=start_date, end_date=end_date, hd=hd)
+        if date:
+            get_image_at_date_from_apod(path, headers, url, boolean_hd=hd)
+        else:
+            get_images_from_apod(headers, url, boolean_hd=hd)
 
 
 if __name__ == '__main__':
